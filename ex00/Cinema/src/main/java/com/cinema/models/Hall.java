7f8a7d0b24a0891caad2fd5c213e0b16cacd641b
Elbarae El-ghannouchi <barae.gh@gmail.com>
2026-03-17 19:22:27 +0000

    Hall model

---
+++ ex00/Cinema/src/main/java/com/cinema/models/Hall.java
@@ -0,0 +1,61 @@
+package com.cinema.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "halls")
+public class Hall {
+    @Id @GeneratedValue
+    @Column(name = "id")
+    private Long    id;
+
+    @Column(name = "serial_number")
+    private Long    serialNumber;
+
+    @Column(name = "seats_count")
+    private int     seatsCount;
+
+    public Hall() {
+    }
+
+    public Hall(Long id, Long serialNumber, int seatsCount) {
+        this.id = id;
+        this.serialNumber = serialNumber;
+        this.seatsCount = seatsCount;
+    }
+
+    public Hall(Long serialNumber, int seatsCount) {
+        this.serialNumber = serialNumber;
+        this.seatsCount = seatsCount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(Long serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public int getSeatsCount() {
+        return seatsCount;
+    }
+
+    public void setSeatsCount(int seatsCount) {
+        this.seatsCount = seatsCount;
+    }
+
+    
+}
